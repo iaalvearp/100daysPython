c26fd4487d278fb728b682ee2c56d1181ea06b96
iaalvearp <iaalvearp@gmail.com>
2024-02-18 04:17:01 -0500

    Day 17 - start

---
+++ day_17/main.py
@@ -0,0 +1,22 @@
+# A class will be named using Pascal case without underscore "_"
+# class User:
+#     def __init__(self, user_id, user_name):
+#         self.user_id = user_id
+#         self.user_name = user_name
+#         self.followers = 0
+#         self.following = 0
+#
+#     def follow(self, user):
+#         user.followers += 1
+#         self.following += 1
+#
+#
+# user_1 = User(123, "AlPes")
+# user_2 = User(124, "Ali")
+#
+# user_1.follow(user_2)
+#
+# print(f"""
+# {user_1.user_name}: following = {user_1.following} | followers = {user_1.followers}
+# {user_2.user_name}: following = {user_2.following} | followers = {user_2.followers}
+# """)
